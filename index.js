6b90696b6954c1efa5d9bdf0004e9e370ac2d00f
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-26 00:48:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+const palindrome1= word.split("").reverse().join("");
+if(word === palindrome1){
+  return true
+}else{
+  return false
+}
 }
 
 /* 
   Add your pseudocode here
 */
 
+// variable = word.split.reverse
+// if element returns a Boolean
+// return result
+
+
 /*
   Add written explanation of your solution here
 */
+
+// reverse word
+// compare reversed word and the initial word
+// return true if the two give the same results
+// otherwise return false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
